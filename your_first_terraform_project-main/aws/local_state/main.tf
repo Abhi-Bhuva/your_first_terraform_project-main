e78935482640ca903e2a26dd1e86d477265e3c96
Abhi-Bhuva <158251667+Abhi-Bhuva@users.noreply.github.com>
2024-10-01 17:18:34 +0530

    Update main.tf

--- your_first_terraform_project-main/aws/local_state/main.tf
+++ your_first_terraform_project-main/aws/local_state/main.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 # Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
+#provider aws {
+  #region = "us-east-1"
+#}
 
 # Create a VPC
 resource "aws_vpc" "example" {

87011dbb9b5f258c2478882f58f459a7dfab94e1
Abhi-Bhuva <158251667+Abhi-Bhuva@users.noreply.github.com>
2024-10-01 14:33:58 +0530

    Add files via upload

---
+++ your_first_terraform_project-main/aws/local_state/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "ap-south-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-08718895af4dfa033"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform_Demo"
+  }
+}
+

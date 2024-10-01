5961c4c3f3a8036cce3e17307872bfbd048e9e01
Abhi-Bhuva <158251667+Abhi-Bhuva@users.noreply.github.com>
2024-10-01 18:12:39 +0530

    Update main.tf

--- your_first_terraform_project-main/aws/local_state/main.tf
+++ your_first_terraform_project-main/aws/local_state/main.tf
@@ -1,18 +1,20 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
-#provider aws {
-  #region = "us-east-1"
+#terraform {
+ # required_providers {
+  #  aws = {
+   #   source  = "hashicorp/aws"
+    #  version = "~> 5.0"
+    #}
+  #}
 #}
 
-# Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+# Configure the AWS Provider
+provider "aws" {
+  access_key = "AKIATZNQFR5PTJMNAOWV"
+  secret_key = "yIKoZWn3ApyHEAlm1alogyxWBPAvzNA8Rmi+Ajlu"
+  region = "us-east-1"
+}
+
+resource "aws_instance" "terraform_example" {
+ami =  "08718895af4dfa033"
+instance_type = "t2.micro"
 }
